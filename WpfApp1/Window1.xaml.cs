9b384247303cf86d2445106442221098cb89bf9e
luxing <luxingzeng@yahoo.com>
2019-05-18 20:55:34 -0400

    added new user interface

--- WpfApp1/Window1.xaml.cs
+++ WpfApp1/Window1.xaml.cs
@@ -10,27 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApp1
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// Window1.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
-         
             InitializeComponent();
-        }
-        void ButtonClicked(object sender, RoutedEventArgs e)
-        {
-            Window1 user = new Window1();
-            /// new to add the code to sent to backend server for the login
-            user.Show();
-            this.Close();
         }
     }
 }
