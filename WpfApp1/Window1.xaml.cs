2cf0cc6b6e5d7e0d563d3b0863b361052a0a6343
luxing <luxingzeng@yahoo.com>
2019-06-13 11:14:16 -0400

    added chrome WebRequest if error try updateing chrome to 75.0

--- WpfApp1/Window1.xaml.cs
+++ WpfApp1/Window1.xaml.cs
@@ -27,6 +27,9 @@ using System.Globalization;
 using RestSharp;
 using System.Net;
 using System.Net.Http;
+using HtmlAgilityPack;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
 
 namespace WpfApp1
 {
@@ -457,25 +460,45 @@ namespace WpfApp1
             Regex regex = new Regex("[^0-9]+");
             e.Handled = regex.IsMatch(e.Text);
         }
+        protected class Payload
+        {
+            [JsonProperty("pid")]
+
+            public string PID { get; set; }
+            [JsonProperty("qty")]
+            public string qty { get; set; }
+        }
         public  void SendPurchaseReques()
         {
+            var payload = new Payload
+            {
+                PID = "34180539",
+                qty = "1"  //https://www.footlocker.com/product/Nike-LeBron-16---Men-s/I1521001.html
+            };
 
-
-            var client = new RestClient("https://www.solebox.com/mein-konto/");
-            RestRequest request = new RestRequest("/", Method.GET);
+            var client = new RestClient("https://www.hibbett.com/on/demandware.store/Sites-Hibbett-US-Site/default/Cart-Modal?format=ajax");
+            RestRequest request = new RestRequest("/", Method.POST);
             CookieContainer cookieJar = new CookieContainer();
-            request.AddHeader("authority", "www.solebox.com");
+            request.AddHeader("authority", "www.hibbett.com");
             request.AddHeader("scheme", "https");
-            request.AddHeader("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3");
+            request.AddHeader("accept", "/");
             request.AddHeader("accept-encoding", "gzip, deflate, br");
             request.AddHeader("accept-language", "it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7");
-            request.AddHeader("cache-control", "max-age=0");
-            request.AddHeader("referer", "https://www.solebox.com/en/home/");
-            request.AddHeader("upgrade-insecure-requests", "1");
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("content-length", "69");
+            request.AddHeader("origin", "https://www.hibbett.com/");
+            request.AddHeader("pragma", "no-cache");
+         //   request.AddHeader("origin", "https://www.hibbett.com/");
+            request.AddHeader("referer", "https://www.hibbett.com/nike-air-max-97-have-a-nike-day-mens-shoe/M0608.html?dwvar_M0608_color=0136&cgid=men-shoes-runningshoes");
+           // request.AddHeader("cache-control", "max-age=0");
+
+          //  request.AddHeader("referer", "https://www.solebox.com/en/home/");
+            ///request.AddHeader("upgrade-insecure-requests", "1");
 
             request.AddHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36");
-
-            Debug.Info("Content: \n{0}", request.ToString());
+            request.AddHeader("x-requested-with", "XMLHttpRequest");
+            request.AddParameter("products", "%5B%7B%22pid%22%3A%2234180539%22%2C%22qty%22%3A%221%22%7D%5D");
+          
 
             // :authority: www.solebox.com
             //  :method: GET
@@ -493,23 +516,70 @@ namespace WpfApp1
             // request.AddHeader("cookie", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36");
             client.CookieContainer = cookieJar;
             //   request.AddHeader("cookie", "cto_lwid=083fed1b-38d9-4fb5-ad3f-17a53f2afbf2; mlid=undefined; displayedCookiesNotification=1; __utmz=1.1557283576.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); sid_key=oxid; px-abgroup=A; px-abper=100; __utmc=1; _fbp=fb.1.1559752431107.1312876194; cto_idcpy=d836f8b2-2cbc-4ab8-b80d-410288d34136; klarnaAddressValidated=0; language=1; __utma=1.1294710284.1557283576.1559758562.1559763477.5; __utmt=1; cto_clc=1; cto_red_atmpt=1; sid=2q7sg85mkhd1fe9vrd8lfraao6; __utmb=1.2.10.1559763477; _px3=b3780d91446af69c8e1d57e6583132a8ade98a4c4b34c84c515a863b6f0257ac:wCMykZK5TCSRg75bdERs3bbh+fRmA0a2GU1FPCOqeuLo8qU/YyoNIG8mG6zMP+dasN/N7MWTJxauftKvvLNV/A==:1000:G6TKS9vchd/M1Xds7ZzDBrStzFgf05Hrup3KHWXhg1qBHAbAOvz7H/2iVa8CCJ9xChfDbLSWVfIzIseD+/apmvLV66bz03QJ3my0K1hE2dwXdOdsGmfy9FP3yjD+KQYIGk2TbGtNDiQccfXkajWtMBJFv0mj1tdR0UCEknT4R8M=");
-           // var response = client.Execute(request);
-
-            //Debug.Info("response.StatusCode: " + ((int)response.StatusCode));
-            //  result = response.Content;
-           // Debug.Info("Content: \n{0}", response.Content);
-
-        }
-        private static void WebRequest()
-        {
-            var client = new RestClient("https://www.solebox.com/mein-konto/");
-            var request = new RestRequest(Method.GET);
-            request.AddHeader("postman-token", "8911afe5-f397-4a32-21d5-20b94c27fa55");
-            request.AddHeader("cache-control", "no-cache");
             var response = client.Execute(request);
 
             Debug.Info("response.StatusCode: " + ((int)response.StatusCode));
+            //  result = response.Content;
             Debug.Info("Content: \n{0}", response.Content);
+
+        }
+        private static void qWebRequest()
+        {
+           
+            var client = new RestClient("https://www.solebox.com/mein-konto/");
+            var request = new RestRequest(Method.GET);
+          //  request.AddHeader("authority", "www.solebox.com");
+         //   request.AddHeader("scheme", "https");
+           // request.AddHeader("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+           // request.AddHeader("accept-encoding", "gzip, deflate, br");
+          //  request.AddHeader("accept-language", "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7");
+           // request.AddHeader("cache-control", "max-age=0");
+           // request.AddHeader("referer", "https://google.com/");
+           // request.AddHeader("upgrade-insecure-requests", "1");
+            request.AddHeader("User-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36");
+ 
+            var response = client.Execute(request);
+            
+
+            Debug.Info("response.StatusCode: " + ((int)response.StatusCode));
+            Debug.Info("Content: \n{0}", response.Content);
+        }
+        private static void WebRequest()
+
+            
+        {
+            var chromeDriverService = ChromeDriverService.CreateDefaultService();
+            chromeDriverService.HideCommandPromptWindow = true;
+            var option = new ChromeOptions();
+            option.AddArguments( "--no-sandbox", "--disable-web-security", "--disable-gpu", "--incognito", "--proxy-bypass-list=*", "--proxy-server='direct://'", "--log-level=3", "--hide-scrollbars"); //"--headless" onlu use if after test is done
+            ChromeDriver driver = new ChromeDriver(chromeDriverService, option);
+
+
+            //Navigate to google page
+            driver.Navigate().GoToUrl("https://www.eastbay.com/");
+
+            //Maximize the window
+            //driver.Manage().Window.Maximize();
+
+            //Find the Search text box using xpath
+           // IWebElement element = driver.FindElement(By.XPath("//*[@title='Search']"));
+
+            //Enter some text in search text box
+           // element.SendKeys("learn-automation");
+
+            //Close the browser
+          //  driver.Close();
+
+            //var html = @"https://www.solebox.com/mein-konto/";
+
+            //HtmlWeb web = new HtmlWeb();
+
+            // var htmlDoc = web.Load(html);
+
+            //var node = htmlDoc.DocumentNode.SelectSingleNode("//head/title");
+
+            // Console.WriteLine("Node Name: " + node.Name + "\n" + node.OuterHtml);
+
         }
 
         private void TaskRemoveClick(object sender, RoutedEventArgs e)
